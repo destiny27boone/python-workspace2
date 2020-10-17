019b1999c5653ec683323e1bfe3400a036228c52
destiny27boone <ddboone27@gmail.com>
2020-10-17 17:20:20 -0500

    Initial commit

---
+++ Variables/Practice.py
@@ -0,0 +1,50 @@
+'''
+
+(name) = (data) 
+
+'''
+
+#Integer
+
+a = 27
+a = 16
+
+#Float
+
+a = 12.5
+a = 30.5
+
+#String
+
+a = "string"
+
+name = "Destiny and Snowball" 
+
+#Boolean
+
+a = True
+
+#List
+
+a = [3, 4, 5, 6]
+
+#Integer
+
+a = 35
+
+A = 50 
+
+var = 47
+
+var = "string"
+
+varTwo = (name[0])
+print (varTwo)
+
+
+list = ["10", "15", "22"]
+
+varThree = (list[1])
+
+var == 13
+
