8f5ee5bd3239838d1838bbe22320c43829678859
destiny27boone <ddboone27@gmail.com>
2020-10-17 21:16:50 -0500

    Variables

---
+++ variable drills/variabledrills.py
@@ -0,0 +1,63 @@
+'''
+
+''' 
+#Integers
+var1 = 5
+print (var1)
+var2 = 10
+print (var2)
+var3 = var2
+print (var3)
+X = 24
+print (X)
+Y = 100
+print (Y)
+
+#Floats
+z = 1.1
+print (z)
+a = 2.75
+print (a)
+b = a
+print (b)
+c = 100.1
+print (c)
+d = 12.3
+print (d)
+
+#strings
+name = "Destiny" 
+print (name)
+e = (name[0])
+print (e)
+f = (name[6])
+print (f)
+g = "chicken"
+print (g)
+h = "Des"
+print (h)
+
+#Booleans
+done = False
+print (done)
+smart = True
+print (smart)
+i = done
+print (i)
+j = False
+print (j)
+almostDone = True
+print (almostDone)
+
+#Lists
+listInt = [2, 3, 4, 5, 6]
+print (listInt[2])
+listString = [23, 22, 27, 16, 30]
+listBoolean = [True, True, True, False, False]
+print (listBoolean[0])
+print (listBoolean[1])
+print (listBoolean[2])
+print (listBoolean[3])
+print (listBoolean[4])
+
+
