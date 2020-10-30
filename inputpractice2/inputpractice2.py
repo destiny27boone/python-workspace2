61418f2cb2ecd2c43e1b018e6daa611944beb84e
destiny27boone <ddboone27@gmail.com>
2020-10-30 13:54:05 -0500

    Input2

---
+++ inputpractice2/inputpractice2.py
@@ -0,0 +1,14 @@
+'''
+Created on Oct 30, 2020
+
+@author: Destiny
+'''
+
+favColor = input("What is your favorite color?")
+color = input("blue")
+
+question = input("What is your name?")
+name = input("Destiny")
+
+age = input("What is your age?")
+age = input("16")
