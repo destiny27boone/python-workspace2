5b0662d6f92c63e135d48cf07e736769737bba8c
destiny27boone <ddboone27@gmail.com>
2020-10-30 13:25:11 -0500

    Input

---
+++ input/inputPractice.py
@@ -0,0 +1,20 @@
+'''
+Created on Oct 30, 2020
+
+@author: Destiny 
+'''
+favColor = input 
+favColor = input("What is your favorite color?")
+favColor = input ("blue")
+
+question = input
+question = input ("What is your name?")
+name = input("Destiny")
+
+question2 = input
+question2 = input("What is your age?")
+
+#integer
+age = 16
+print (age)
+ 
