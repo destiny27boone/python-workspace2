54bc4bcf7b8ad02875e97fcadadaf8e6fac47aeb
destiny27boone <ddboone27@gmail.com>
2020-10-30 14:29:21 -0500

    Input drills

---
+++ inputDrills/inputDrills.py
@@ -0,0 +1,13 @@
+
+
+favAnimal = input("What is your favorite animal?")
+
+favNum = input("What is your favorite number?")
+favNum = int()
+
+school = input("What is your school's name?")
+
+instructor = input("What is your instructors name?")
+
+ans = input ("What is 2 + 2?")
+ans = int()
