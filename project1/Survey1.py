48fa6523ca30a72dc44252cdfbbea35d64db0475
destiny27boone <ddboone27@gmail.com>
2020-10-31 13:21:44 -0500

    survey

---
+++ project1/Survey1.py
@@ -0,0 +1,20 @@
+'''
+Make a program that can take survey information.
+
+@author: Destiny Boone
+'''
+
+name = input("What is your name?")
+print (name)
+
+color = input("What is your favorite color?")
+print (color)
+
+city = input("What city did you grow up in?")
+print (city)
+
+friend = input("What is your best friends name?")
+print (friend)
+
+hair = input("What color hair do you have?")
+print (hair)
