e69fc2e6b7917edeadc6469d4e3a43764065720f
destiny27boone <ddboone27@gmail.com>
2020-11-16 17:39:14 -0600

    mathematical operation

---
+++ Mathematical operators/MathematicalOperators.py
@@ -0,0 +1,57 @@
+'''
+Created on Nov 16, 2020
+
+@author: Destiny Boone
+'''
+
+y = "name" + " your"
+
+a = 16 
+b = 27
+c = a + b
+
+d = 45
+e = 25
+f = d - e
+
+g = 70.56
+h = 120.23
+i = g - h 
+
+j = 89
+k = 26
+l = j * k 
+
+m = "toast"
+n = 5
+o = m *  n
+
+
+p = 224
+q = 2
+r = p / q 
+
+s = 45.36
+t = 80.65
+u = s / t 
+
+
+v = 85
+w = 101
+x = v % w 
+
+
+y = 504
+z = 608
+ab = y // z 
+
+bc = 965
+cd = 397
+de = bc ** cd 
+
+
+#12) Make any type of operation with a common SYNTAX error
+ef = 25.4
+fg = 45.85
+gh == ef / fg 
+
