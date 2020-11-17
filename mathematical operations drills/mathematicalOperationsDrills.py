3fd5913a96ae66a3646f67e9f1fbd19ddc68503e
destiny27boone <ddboone27@gmail.com>
2020-11-16 18:09:09 -0600

    mathematical operations drills

---
+++ mathematical operations drills/mathematicalOperationsDrills.py
@@ -0,0 +1,83 @@
+'''
+Created on Nov 16, 2020
+
+@author: Destiny Boone 
+'''
+
+
+math = 4 + 5
+print (math) 
+
+q = 6 + 10 
+print (q)
+
+w = math + q 
+print (w)
+
+string = "concat" + " is fun"
+print (string) 
+
+first = "Destiny" 
+last = " Boone" 
+e = first + last 
+print (e)
+
+r = 4 - 5
+print (r)
+
+t = 6 - 10 
+print (t)
+
+y = t - r 
+print (y)
+
+u = 4 * 5
+print (u)
+
+i = 5 * 3
+print (i)
+
+o = i * u 
+print (o)
+
+string = "string" * 3
+print (string)
+
+p = 4 / 5
+print (p)
+
+a = 5 / 3
+print (a)
+
+s = a / p 
+print (s)
+
+d = 5 % 3
+print (d)
+
+f = 234 % 3 
+print (f)
+
+g = f % d 
+print (g)
+
+
+h = 5 // 3
+print (h)
+
+j = 234 // 3
+print(j)
+
+k = h // j 
+print (k)
+
+l = 2 ** 2
+print (l)
+
+z = 3 ** 3
+print (z)
+
+x = l ** z 
+print (x) 
+
+
