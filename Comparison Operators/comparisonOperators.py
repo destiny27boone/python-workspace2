e3e55443300d5294d6872deb23fa870768cc769b
destiny27boone <ddboone27@gmail.com>
2020-11-17 17:41:54 -0600

    Comparison Operators

---
+++ Comparison Operators/comparisonOperators.py
@@ -0,0 +1,43 @@
+'''
+Created on Nov 17, 2020
+
+@author: Destiny Boone
+'''
+
+a = "String"
+b = "String"
+c = a == b  
+print (c)
+
+x = 4
+y = 4
+z = x == y 
+print (z)
+
+d = 10
+e = 3
+f = d > e 
+print (f)
+
+g = 6
+h = 115
+i = g < h
+print (i) 
+
+j = 60
+k = 5 
+l = j >= k 
+print (l)
+
+m = 30 
+n = 127 
+o = m <= n
+print (o)
+
+
+#7) Make a comparison with any operator, with one of the common SYNTAX errors.
+
+p = 70 
+q = 75 
+r = Q == p 
+
