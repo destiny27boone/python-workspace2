5c4cfadb4abed6f7055097e56da92d7750cf18d1
destiny27boone <ddboone27@gmail.com>
2020-11-17 18:58:03 -0600

    Debug ComparisonOperators

---
+++ Debug Comparison Operators/DebugComparisonOperators.py
@@ -0,0 +1,71 @@
+'''
+Created on Nov 17, 2020
+
+@author: Destiny Boone
+'''
+'''
+This function compare a to b.
+
+-If a is less than b, the function prints "A is less than B."
+-If a is equal to b, the function prints "A is equal to B."
+-If a is more than b, the function prints "A is greater than B."
+'''
+def comparisonDrill(a, b):
+    if(a < b):
+        print("A is less than B.")
+    if(a == b):
+        print("A is equal to B.")
+    if(a > b):
+        print("A is greater than B.")
+    
+    return 
+
+comparisonDrill(2, 3)
+comparisonDrill(3, 3)
+comparisonDrill(4, 3)
+
+'''
+This function compares userTest to correctAnswer
+
+-If the variables are the same, the function prints "CORRECT!"
+'''
+def comparisonDrillTwo(userText, correctAnswer):
+    if(userText == correctAnswer):
+        print("CORRECT!")
+    
+    return 
+
+comparisonDrillTwo("YES", "YES")
+comparisonDrillTwo("NO", "YES")
+
+'''
+The goal of this function is to calculate the 
+total of an order after tax and shipping.
+
+-If the subtotal is less than 15, the function makes shipping 5.
+-If the subtotal is more or equal to 15, the function makes shipping 10.
+-If the subtotal is more than 100, the function makes shipping 0.
+
+The function should print and
+return the total
+'''
+def shippingAndTax(subtotal):
+    shipping = 0
+    tax = .10
+    
+    if(subtotal < 15):
+        shipping = 5
+    if(subtotal >= 15):
+        shipping = 10
+    if(subtotal > 100):
+        shipping = 0
+    
+    taxTotal = subtotal * tax
+    total = subtotal + taxTotal + shipping
+    print("The total is: ", total)
+    return total
+
+shippingAndTax(10)
+shippingAndTax(15)
+shippingAndTax(20)
+shippingAndTax(101)
