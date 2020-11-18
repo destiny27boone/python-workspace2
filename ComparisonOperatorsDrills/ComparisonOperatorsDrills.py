2db0566f5a8b8d094c2bb08e0ef12ebd16ce4e7d
destiny27boone <ddboone27@gmail.com>
2020-11-17 18:39:45 -0600

    ComparisonOperatorDrills

---
+++ ComparisonOperatorsDrills/ComparisonOperatorsDrills.py
@@ -0,0 +1,110 @@
+'''
+Created on Nov 17, 2020
+
+@author: Destiny Boone
+'''
+
+b = "dog" == "cat" 
+
+c = (1.24 == 1.25) 
+
+d = True == False 
+
+e = [1,2,3] == [1,2,3]
+
+f = 1 <= 2 
+
+g = 1 >= 2
+
+h = 1.66 >= 1.67
+
+i = 1.66 <= 1.67
+
+j = 1 < 2 
+
+k = 1 > 2 
+
+l = 11 / 3 < 100 / 33
+
+m = 9 * 7 > 8 * 8
+
+
+o = 8 * 3 
+p = 56 / 2
+q = o > p 
+
+#1) 4 == 5
+False 
+
+#2) 5 == 5
+True
+
+#3) 3 == 3
+True
+
+#4) a = 2
+# ) a == 2
+True
+#5) a = 3
+# ) b = 2
+# ) a == b
+False
+
+#6) a = 4
+# ) b = 4
+# ) a == b
+True
+#7) a = 6
+# ) b = (12/2)
+# ) a == b
+True
+#8) a = (3/3)
+# ) b = (13/13)
+# ) a == b
+True
+#9) a = 3
+# ) b = 9 / 3
+# ) a == b
+True
+#10) a = "word"
+#  ) b = "Word"
+#  ) a == b
+True
+#11) (10/5) == (12/6)
+True
+#12) (14/7) == (15/5)
+False
+#13) a = 10
+#  ) b = 5
+#  ) (a/b) == 2
+True
+#14) a = 10
+#  ) b = 5
+#  ) (a/b) == (12/6)
+True
+#15) a = 10
+#  ) b = 5
+#  ) c = 12
+#  ) d = 6
+#  ) (a/b) == (12/6)
+True
+#16) a = "something"
+#  ) b = "some thing"
+#  ) a == b
+False
+#17) a = "word"
+#  ) b = "word"
+#  ) a == b
+True 
+#18) a = True
+#  ) a == True
+True
+#19) a = True
+#  ) b = False
+#  ) a == b
+False
+#20) a = True
+#  ) A = False
+#  ) b = False
+#  ) A == b
+True 
