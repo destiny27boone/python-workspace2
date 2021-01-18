d238d26bbc728b4a376887fb415e5b6aff6845f8
destiny27boone <ddboone27@gmail.com>
2021-01-18 15:29:03 -0600

    logicalOperation

---
+++ logicalOperators/logicalOperators.py
@@ -0,0 +1,33 @@
+'''
+Created on Jan 18, 2021
+
+@author: Destiny Boone
+
+'''
+
+one = True
+two = False
+a = one and two 
+print(a)
+
+
+four = 4
+five = 5 
+a = four and five 
+print(a)
+
+
+three = 4 
+two = 6
+b = three and two
+print(b)
+
+
+not 7 = 7
+print(7)
+
+
+ten = 8
+one === 14
+d = ten and one
+
