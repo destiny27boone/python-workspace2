2a9276ffdeaf527e7351cae9572c3551b72f280f
destiny27boone <ddboone27@gmail.com>
2021-02-09 18:31:57 -0600

    condStatePractice

---
+++ conditionalStatements/conditionalStatements.py
@@ -0,0 +1,36 @@
+'''
+Created on Feb 9, 2021
+
+@author: Destiny Boone
+'''
+
+ 
+a = 6 
+if(a > 5):
+    print("a is more than 5.")
+
+b = True 
+if(True):
+    print ("b is True.")
+    
+c = 15
+if(c > 0):
+    print ("c is positive.")
+else: 
+    print ("c is negative.")
+
+
+
+d = 20
+if(d > 0):
+    print("d is positive.")
+else:
+    print("d is zero.")
+     
+
+
+g = 90 
+if(g < 100):
+    prin("g is negative")
+
+
