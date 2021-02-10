1deb53bfc06aa29a3d29e5f26185dab8c1d352fb
destiny27boone <ddboone27@gmail.com>
2021-02-09 19:09:48 -0600

    condStateDrills

---
+++ condStateDrills/condStateDrills.py
@@ -0,0 +1,51 @@
+'''
+Created on Feb 9, 2021
+
+@author: Destiny Boone
+'''
+
+
+grade = 100
+if(grade >= 80):
+    print("passing")
+elif(grade < 80 >=60):
+    print("probation")
+else:
+    print("failing")
+
+
+a = 30
+if( a >= 100 <= -100):
+    print("a is a three digit number")
+elif(a >= 100 >= -100): 
+    print("a is not a three digit number") 
+    
+
+age = 16 
+if(age > 18):
+    print("You can vote")
+else:
+    print("You cannot vote.")
+
+
+
+answer = "a, b, c, d"
+if(answer == "a, b, c, d"):
+    print("wrong")
+elif(answer == c):
+    print("Correct")
+
+
+
+F = False
+T = True 
+if(F == False, T == True):
+    print("both")
+elif(F == True, T == False):
+    print ("Only one.")   
+else:
+    print("Neither") 
+
+
+
+
