fc8c3a711084d2238261c084f3730af7d4500c84
destiny27boone <ddboone27@gmail.com>
2021-03-09 21:50:43 -0600

    loopsDrills

---
+++ loopsDrills/loopsDrills.py
@@ -0,0 +1,98 @@
+'''
+Created on Mar 9, 2021
+
+@author: Destiny Boone
+
+while (condition):
+    code block
+for (literal) in (iterable  [lists or strings]):
+    code block
+'''
+w = 4 
+while w > 1:
+    print(w)
+    w -= 1
+
+r = 14
+while r > 20:
+    print(r)
+    r = 1 
+
+q = 55
+while q > 50:
+    print(q)
+    q = 50 
+
+'''For Loops'''
+
+sports = "tennis","football","baseball"
+for sports in sports:
+    print(sports)
+ 
+f = "willow"
+for w in f:
+    
+    print(f[0])
+    print(f[1])
+    print(f[2])
+    print(f[3])
+    print(f[4])
+    print(f[5])
+
+
+m = ["Secret of the wings","Avatar","The pirate fairy","Wall-e","Enchanted"]
+if m == "Avatar":
+    print("Secret of the wings","The pirate fairy","Wall-e","Enchanted")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
