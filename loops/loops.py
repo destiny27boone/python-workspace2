cc7c5a72672f918c084f78900051cd8148f6be4a
destiny27boone <ddboone27@gmail.com>
2021-03-09 20:43:08 -0600

    loops

---
+++ loops/loops.py
@@ -0,0 +1,62 @@
+'''
+Created on Mar 9, 2021
+
+@author: Destiny Boone
+
+while (condition):
+    code block
+
+for (literal) in (iterable  [lists or strings]):
+    code block
+'''
+'''
+while 
+i = 0
+while(i < 10):
+     print(i)
+     i = i + 1
+
+for   
+var = [1,2,3,4,5,6]
+
+for x in var: 
+    print(x)
+''' 
+
+x = 0 
+while(x <= 20):
+    print(x)
+    x = x + 1
+
+x = 0
+while(x < 100):
+    x = x + 1
+    print(x)
+if(x == 15):
+    print(x)
+
+
+s = [1,2,3]
+for s in s:
+    print(s)
+
+
+a = ["Michael","Chris", "Nino"]
+for a in a:
+    print(a)
+if(a == "Chris"):
+    print("Michael","Nino")
+
+
+b = 24
+while(b < 10):
+    prin(b)
+    b = b + 2
+
+
+n  [2,20,12,15]
+
+for n in n:
+    print(n)
+
+
