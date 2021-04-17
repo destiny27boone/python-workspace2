bbced7236c66fc8d8ff2a08a8a753f91e1154735
destiny27boone <ddboone27@gmail.com>
2021-04-17 00:06:30 -0500

    Functions

---
+++ functions/functions.py
@@ -0,0 +1,90 @@
+'''
+Created on Apr 16, 2021
+
+@author: ITAUser
+
+def [nameOfFunction] (parameter):
+    code block
+    return [variable/data]
+    
+start with letter
+camel casing
+any length, but short as possible
+no key words 
+'''
+
+
+def sumTwoNums(numOne, numTwo):
+    sum = numOne + numTwo
+    return True
+
+x = True 
+y = True 
+num = sumTwoNums (x,y)
+print(sum)
+
+
+def units(gallons, cups):
+    units = gallons * 16 
+    return cups 
+gallons = 4 
+cups = 16
+num = units (gallons,cups) 
+print (units) 
+
+
+
+#3) Make a function of any any kind with a common SYNTAX error. Then, call the
+#function.
+#Print what the function returns.
+
+
+def sumTwoNums2(numTwo, numThree):
+    sum2 = numTwo + numThree  
+    returnn True
+b = True
+g = False
+num2 = sumTwoNums (b,g)
+print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
