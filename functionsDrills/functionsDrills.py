c20e16af176e81ed8bb0b9f3b04f4ae402e17dd6
destiny27boone <ddboone27@gmail.com>
2021-04-21 13:23:14 -0500

    functionDrills

---
+++ functionsDrills/functionsDrills.py
@@ -0,0 +1,75 @@
+'''
+Created on Apr 21, 2021
+
+@author: ITAUser
+'''
+
+
+
+
+
+def sub_two_numbers:
+    diffOfTwoNumbers = 10 - 8
+    return diffOfTwoNumbers
+print(diffOfTwoNumbers)
+
+
+
+
+def operations(12):
+    QuotProdSum = 12/2 * 77 + 10,00
+    return QuotProdSum
+print(return)
+    
+
+
+
+def truth(12,12):
+    twoInt = 12 == 12
+    return True 
+print(True)
+
+
+
+def bigger_num(16,27):
+    twoIntNum = if (27 > 16) or (16 > 27): 
+    largest = 27
+    return largest 
+print(largest)
+    
+
+
+
+def two_strings("water", "bottle"):
+    sumTwoNum = "water" + "bottle"
+    return sumTwoNum
+print(sumTwoNum)
+
+
+
+def true_false(32,30,27):
+    if (32 = 30) or (32 = 27)
+    return True 
+else 
+return False 
+print(return)
+
+
+
+def Destiny
+print(def)
+
+
+
+
+def string_colors("red"):
+    "red" = favorite color 
+    print("That's my favorite color")
+
+
+def int_positive(100):
+    while(100 != 0):
+        print(100)
+      100 = 100 - 1 
+
+
